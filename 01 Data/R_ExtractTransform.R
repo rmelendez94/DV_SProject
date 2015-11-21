943b57e2d5f6cb44b1640897b2e44ca6f93c8fef
hyunji-seo <hjseo767@gmail.com>
2015-11-21 13:44:49 -0600

    Incomplete Rmd

--- 01 Data/R_ExtractTransform.R
+++ 01 Data/R_ExtractTransform.R
@@ -1,7 +1,7 @@
 require(dplyr)
 
 # Use getwd() and use your filepath to 01 Data
-setwd("C:\\Users\\hyunji\\Documents\\DataVisualization\\DV_TProject1\\01 Data\\CSVs")
+setwd("C:\\Users\\hyunji\\Documents\\DataVisualization\\DV_SProject\\01 Data\\CSVs")
 
 file_path <- "bank-additional.csv"
 
